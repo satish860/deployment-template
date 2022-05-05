59bb52c6975245e3b9b683a41cf8e5760a087c21
satish <satish860@gmail.com>
2022-05-05 14:26:11 +0530

    Dotnet Example

---
+++ tests/mylibrary/UnitTest1.cs
@@ -0,0 +1,19 @@
+using MyLibrary;
+using Xunit;
+
+namespace Mylibrary;
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldReturnHelloWorldThreeTimes()
+    {
+        const string expected = @"Hello World
+Hello World
+Hello World";
+        var sut = new HelloWorldPrinter();
+
+        var str = sut.GetHellWorld(3);
+
+        Assert.Equal(expected, str);
+    }
+}
